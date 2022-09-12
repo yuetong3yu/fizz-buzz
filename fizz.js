6b8007769955e242712abf8cea8399382b53228a
yuetong3yu <yuetong3yu@gmail.com>
2022-09-12 22:17:23 +0800

    Complete the function, pass all the tests

--- fizz.js
+++ fizz.js
@@ -1,5 +1,10 @@
 export function fizz(n) {
-  return undefined;
+  const ERROR_MESSAGE = "Input Error";
+  if (typeof n !== "number") return ERROR_MESSAGE;
+  if (n <= 0) return ERROR_MESSAGE; 
+  if (n % 15 === 0) return "FizzBuzz";
+  if (n % 3 === 0) return "Fizz";
+  if (n % 5 === 0) return "Buzz";
 }
 
 export function sum(n1, n2) {
--- fizz.test.js
+++ fizz.test.js
@@ -9,17 +9,17 @@ test("Fizz: 3 and 5 both", () => {
   expect(fizz(30)).toBe("FizzBuzz")
 })
 
-test.skip("Fizz: 3", () => {
+test("Fizz: 3", () => {
   expect(fizz(3)).toBe("Fizz");
   expect(fizz(9)).toBe("Fizz");
 })
 
-test.skip("Fizz: 5", () => {
+test("Fizz: 5", () => {
   expect(fizz(10)).toBe("Buzz");
   expect(fizz(5)).toBe("Buzz");
 })
 
-test.skip("Fizz: Input error", () => {
+test("Fizz: Input error", () => {
   const error = "Input Error"
   expect(fizz(0)).toBe(error);
   expect(fizz(-1)).toBe(error);
